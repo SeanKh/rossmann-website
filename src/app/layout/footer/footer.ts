a66ce6c4ac77c05588315d6c052244943c17ac31
Shukhrat <sh.khaydarov96@gmail.com>
2026-07-09 23:23:29 +0500

    translations for contact part working perfectly

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslocoPipe
+  ],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
 export class Footer {
 
   currentYear = new Date().getFullYear();
+
+  scrollTo(id: string): void {
+
+    const element = document.getElementById(id);
+
+    if (element) {
+
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+
+    }
+
+  }
 
 }

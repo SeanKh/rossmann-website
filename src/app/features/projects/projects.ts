bc4b0e91022c23c60711da77cea3071881fe0911
Shukhrat <sh.khaydarov96@gmail.com>
2026-07-09 23:18:00 +0500

    translations for projects part working perfectly

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [TranslocoPipe],
   templateUrl: './projects.html',
   styleUrl: './projects.scss'
 })
